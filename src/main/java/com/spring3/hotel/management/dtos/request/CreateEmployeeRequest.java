e0f2de28925db35ead815c200a0a8f87c2a8b467
LinhIsMee <linh.pthth@gmail.com>
2025-03-29 20:34:27 +0700

    Thêm lớp CreateEmployeeRequest: định nghĩa các thuộc tính cho yêu cầu tạo nhân viên

---
+++ src/main/java/com/spring3/hotel/management/dtos/request/CreateEmployeeRequest.java
@@ -0,0 +1,20 @@
+package com.spring3.hotel.management.dtos.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CreateEmployeeRequest {
+    private String name;
+    private String email;
+    private String phone;
+    private String department; // Sẽ chuyển đổi thành enum Department
+    private String position;   // Sẽ chuyển đổi thành enum Position
+    private String joinDate;   // Định dạng YYYY-MM-DD
+    private Boolean status;
+} 
