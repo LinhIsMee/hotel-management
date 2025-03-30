bc8fb36902f996d6af33ec55f76c1b5362e3fe7a
LinhIsMee <linh.pthth@gmail.com>
2025-03-30 22:15:43 +0700

    Update discount API path to match with API standard format

--- src/main/java/com/spring3/hotel/management/controllers/DiscountController.java
+++ src/main/java/com/spring3/hotel/management/controllers/DiscountController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/discounts")
+@RequestMapping("/api/v1/discounts")
 @CrossOrigin(origins = "*")
 public class DiscountController {
 
